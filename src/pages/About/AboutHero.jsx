0e012b25ccb492c0204ad7b3a7ef9128e3bea3a9
johnabednego <jilimajohnabednego@gmail.com>
2023-12-03 14:39:53 +0000

    done

--- src/pages/About/AboutHero.jsx
+++ src/pages/About/AboutHero.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import NavWithTitle from '../../components/Nav/NavWithTitle'
 import woman from './assets/woman.svg'
+import what from './assets/what.svg'
+
 const AboutHero = () => {
   return (
     <div className=' pb-[60px]'>
-       <NavWithTitle title="About Us"/>
-       <div className='w-full h-[513px] pt-[195px]'>
-        <img src={woman} alt="" />
-       </div>
+      <NavWithTitle title="About Us" />
+      <div className='w-full  pt-[195px]'>
+        {/* <img src={woman} alt="" className='' /> */}
+        <div className=' w-full h-[513px] flex items-center  bg-about_woman bg-center bg-cover'>
+          <div className=' w-full text-center flex flex-col gap-[27px] px-3'>
+            <h1 className=' text-[16px] sm:text-[20px] text-white'>We love what we do and we love helping others succeed at what they love to do.</h1>
+            <h1 className=' text-[14px] sm:text-[18px] text-[#FAFAFA]'>Prince Vac, <span className=' text-[#A8CF38]'>CEO</span></h1>
+          </div>
+        </div>
+      </div>
+     <div className=' w-full flex items-center justify-center'>
+     <img src={what} alt="" className=' mt-[-8%]' />
+     </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
         backgroundImage: {
           nav_bg:"url('/src/components/Nav/assets/nav_bg.svg')",
+          about_woman:"url('/src/pages/About/assets/woman.svg')",
         },
       screens: {
         'xm': '500px',
