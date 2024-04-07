bd41786fcd3baaa249ca61b1cb6433a58a8f3a4d
johnabednego <jilimajohnabednego@gmail.com>
2024-04-07 17:09:25 +0000

    done

--- src/components/Dashboard/Bookings/ViewBooking.jsx
+++ src/components/Dashboard/Bookings/ViewBooking.jsx
@@ -2,57 +2,99 @@ import React from 'react'
 
 const ViewBooking = () => {
   return (
-    <div className=' w-full xm:w-[480px]  sm:w-[528px] md:w-[628px]  lg:w-[828px] py-[57px] px-[65px] h-[757px] shadow-input bg-white rounded-br-[30px] rounded-tl-[30px]'>
-      <div className='w-full flex items-center justify-between gap-4'>
-        <h1 className=' w-full flex-1 font-bold text-[36px] text-[#51336A]'>Items</h1>
-        <div className=' flex w-full flex-[1.6] items-center justify-between gap-4 -mt-20'>
-          <div className=' whitespace-nowrap h-[30px] rounded-[50px] border-[1px] border-[#4FBF26] px-2 py-[5px] bg-[#D6FBC9] flex items-center justify-center text-center font-bold text-[#4FBF26] text-[12px]'>Online Payment</div>
-          <h1 className='font-bold text-[32px]'>₵50</h1>
-        </div>
-      </div>
-      {/** Items*/}
-      <div className=' mt-[14px] flex flex-wrap gap-[38px]'>
-        <div className=" items-center w-fit gap-[30px] mt-4 bg-gray-200 flex relative  rounded-full px-3 py-1">
-          <span>Fridge</span>
-          <h1 name="" id="" className={` ml-2 flex items-center justify-center text-center  px-2  focus:outline-[#51336A] m-0 placeholder-[#707070] text-[15px] text-[#707070] font-normal bg-white  rounded-full `}>1</h1>
-        </div>
+    <div className=' w-full flex flex-col gap-[20px] xm:w-[480px]  sm:w-[528px] md:w-[628px]  lg:w-[600px] py-[20px] px-[30px] shadow-input bg-white rounded-br-[30px] rounded-tl-[30px]'>
+      
+      <div className=' w-full flex  items-center justify-end  gap-4'>
+        <div className=' whitespace-nowrap h-[30px] rounded-[50px] border-[1px] border-[#4FBF26] px-2 py-[5px] bg-[#D6FBC9] flex items-center justify-center text-center font-bold text-[#4FBF26] text-[12px]'>Online Payment</div>
+        <h1 className='font-bold text-[32px]'>₵50</h1>
       </div>
 
-      {/**Date(s) */}
-      <div className='w-full flex justify-between flex-wrap gap-8 mt-[60px]'>
-        {/**Period */}
-        <div className=' flex flex-col gap-1'>
-          <h1 className='font-bold text-[26px] text-[#51336A]'>Period</h1>
-          <h1 className=' text-[18px]'>1 Month</h1>
+      <div className=' w-full flex flex-col gap-[30px]'>
+        {/** Items*/}
+        <div className='w-full flex flex-col'>
+          <h1 className=' w-full font-bold text-[18px] md:text-[20px] lg:text-[24px] text-[#51336A]'>Items</h1>
+          <div className=' flex flex-wrap gap-[10px]'>
+
+            <div className=" items-center w-fit gap-[30px] mt-4 bg-gray-200 flex relative  rounded-full px-3 py-1">
+              <span>Fridge</span>
+              <h1 name="" id="" className={` ml-2 flex items-center justify-center text-center  px-2  focus:outline-[#51336A] m-0 placeholder-[#707070] text-[15px] text-[#707070] font-normal bg-white  rounded-full `}>1</h1>
+            </div>
+
+            <div className=" items-center w-fit gap-[30px] mt-4 bg-gray-200 flex relative  rounded-full px-3 py-1">
+              <span>Iron</span>
+              <h1 name="" id="" className={` ml-2 flex items-center justify-center text-center  px-2  focus:outline-[#51336A] m-0 placeholder-[#707070] text-[15px] text-[#707070] font-normal bg-white  rounded-full `}>1</h1>
+            </div>
+
+            <div className=" items-center w-fit gap-[30px] mt-4 bg-gray-200 flex relative  rounded-full px-3 py-1">
+              <span>Luggage</span>
+              <h1 name="" id="" className={` ml-2 flex items-center justify-center text-center  px-2  focus:outline-[#51336A] m-0 placeholder-[#707070] text-[15px] text-[#707070] font-normal bg-white  rounded-full `}>1</h1>
+            </div>
+
+            <div className=" items-center w-fit gap-[30px] mt-4 bg-gray-200 flex relative  rounded-full px-3 py-1">
+              <span>Bucket</span>
+              <h1 name="" id="" className={` ml-2 flex items-center justify-center text-center  px-2  focus:outline-[#51336A] m-0 placeholder-[#707070] text-[15px] text-[#707070] font-normal bg-white  rounded-full `}>1</h1>
+            </div>
+
+            <div className=" items-center w-fit gap-[30px] mt-4 bg-gray-200 flex relative  rounded-full px-3 py-1">
+              <span>Other(s)</span>
+              <h1 name="" id="" className={` ml-2 flex items-center justify-center text-center  px-2  focus:outline-[#51336A] m-0 placeholder-[#707070] text-[15px] text-[#707070] font-normal bg-white  rounded-full `}>12</h1>
+            </div>
+
+            <div className=" items-center w-fit gap-[30px] mt-4 bg-gray-200 flex relative  rounded-full px-3 py-1">
+              <span>Cup</span>
+              <h1 name="" id="" className={` ml-2 flex items-center justify-center text-center  px-2  focus:outline-[#51336A] m-0 placeholder-[#707070] text-[15px] text-[#707070] font-normal bg-white  rounded-full `}>1</h1>
+            </div>
+          </div>
         </div>
 
-        {/**Pickup Date */}
-        <div className=' flex flex-col gap-1'>
-          <h1 className='font-bold text-[26px] text-[#51336A]'>Pickup Date</h1>
-          <h1 className=' text-[18px]'>11th Jan. 2024</h1>
+        {/**Date(s) */}
+        <div className='w-full flex justify-between flex-wrap gap-8'>
+          {/**Period */}
+          <div className=' flex flex-col gap-1'>
+            <h1 className='font-bold text-[18px] md:text-[20px]  text-[#51336A]'>Period</h1>
+            <h1 className=' text-[14px] sm:text-[16px]'>1 Month</h1>
+          </div>
+
+          {/**Pickup Date */}
+          <div className=' flex flex-col gap-1'>
+            <h1 className='font-bold text-[18px] md:text-[20px]  text-[#51336A]'>Pickup Date</h1>
+            <h1 className=' text-[14px] sm:text-[16px]'>11th Jan. 2024</h1>
+          </div>
+
+          {/**Delivery Date*/}
+          <div className=' flex flex-col gap-1'>
+            <h1 className='font-bold text-[18px] md:text-[20px]  text-[#51336A]'>Delivery Date</h1>
+            <h1 className={` text-[18px] border-dashed border-b-[2px] border-[#51336A] h-[15px]`}></h1>
+          </div>
         </div>
 
-        {/**Delivery Date*/}
-        <div className=' flex flex-col gap-1'>
-          <h1 className='font-bold text-[26px] text-[#51336A]'>Delivery Date</h1>
-          <h1 className=' text-[18px]'>Null</h1>
+        {/**Student Location */}
+        <div className='w-full flex justify-between flex-wrap gap-4'>
+          {/**School */}
+          <div className=' flex flex-col gap-1'>
+            <h1 className='font-bold text-[18px] md:text-[20px]  text-[#51336A]'>School</h1>
+            <h1 className=' text-[14px] sm:text-[16px]'>UG</h1>
+          </div>
+
+          {/**Hall or Hostel */}
+          <div className=' flex flex-col gap-1'>
+            <h1 className='font-bold text-[18px] md:text-[20px]  text-[#51336A]'>Hall or Hostel</h1>
+            <h1 className=' text-[14px] sm:text-[16px]'>11th Jan. 2024</h1>
+          </div>
+
+          {/**Emergency Contact */}
+          <div className=' flex flex-col gap-1'>
+            <h1 className='font-bold text-[18px] md:text-[20px]  text-[#51336A]'>Emergency Contact</h1>
+            <h1 className=' text-[14px] sm:text-[16px]'>+233247157301</h1>
+          </div>
         </div>
       </div>
 
-      {/**Student Location */}
-      <div className='w-full flex justify-between flex-wrap gap-4 mt-[60px]'>
-        {/**School */}
-        <div className=' flex flex-col gap-1'>
-          <h1 className='font-bold text-[26px] text-[#51336A]'>School</h1>
-          <h1 className=' text-[18px]'>UG</h1>
+       {/**Request */}
+       <div className=' w-full mt-4 flex items-center justify-center'>
+          <button type="button" className=" transform duration-300 ease-in-out rounded-[30px] bg-black  hover:opacity-100 hover:bg-[#1f1e1f] text-white font-bold py-2 px-4">Request Delivery</button>
         </div>
 
-        {/**Hall or Hostel */}
-        <div className=' flex flex-col gap-1'>
-          <h1 className='font-bold text-[26px] text-[#51336A]'>Hall or Hostel</h1>
-          <h1 className=' text-[18px]'>11th Jan. 2024</h1>
-        </div>
-      </div>
     </div>
   )
 }
--- src/components/Dashboard/Bookings/ViewBookingModal.jsx
+++ src/components/Dashboard/Bookings/ViewBookingModal.jsx
@@ -16,7 +16,7 @@ const ViewBookingModal = ({ bookedView, setBookedView }) => {
                 onClick={hideModal}
                 className=" cursor-pointer pt-0 h-full justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0  z-[70] outline-none focus:outline-none scrollbar-hide"
             >
-                <div id='modal' className="relative mt-[500px] xm:mt-[400px] sm:mt-[300px] xl:mt-[200px] mb-3  mx-2 sm:mx-0 ">
+                <div id='modal' className="relative  mb-3  mx-2 sm:mx-0 ">
                     {/*content*/}
                     <div className=" w-full cursor-default border-0  relative  rounded-br-[30px] rounded-tl-[30px] shadow-lg flex flex-col  bg-white outline-none focus:outline-none">
                         <ViewBooking />
