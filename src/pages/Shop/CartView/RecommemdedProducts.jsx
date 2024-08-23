c4468c25d96e7d41507f89acc0c0f046d84aff4a
johnabednego <jilimajohnabednego@gmail.com>
2024-08-24 03:30:46 +0430

    done

--- src/pages/Shop/CartView/RecommemdedProducts.jsx
+++ src/pages/Shop/CartView/RecommemdedProducts.jsx
@@ -10,7 +10,7 @@ const RecommemdedProducts = () => {
                 <div className='flex gap-3'>
                     <img src={bag2} alt="bag2" className=' w-[100px] h-[100px] object-cover object-center' />
                     <div className='flex flex-col gap-5'>
-                        <h1 className=' text-[18px]'>Three in One Luggage</h1>
+                        <h1 className=' text-[16px] sm:text-[18px]'>Three in One Luggage</h1>
                         <button className=' hover:opacity-80 px-[10px] w-fit h-[35px] rounded-[6px] bg-gradient-to-r from-[#51336A] to-[#9F64D0] font-bold text-[12px] text-white border-solid border-[1px] border-[#51336A] transition-all transform ease-in-out duration-300'>Add to Cart</button>
                     </div>
                 </div>
